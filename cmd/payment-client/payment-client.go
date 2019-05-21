fde930adb0f6dfd4869459974963daea69efbad9
melkor <melkor666free@gmail.com>
2019-05-21 14:52:29 +0200

    credit card number is int64

--- cmd/payment-client/payment-client.go
+++ cmd/payment-client/payment-client.go
@@ -3,22 +3,27 @@ package main
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/melkor/test-thrift/client"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
+
+	"github.com/melkor/test-thrift/client"
+	"github.com/melkor/test-thrift/payment"
 )
 
 var (
 	addr       = pflag.StringP("addr", "h", "localhost:9090", "Address to listen to")
 	protocol   = pflag.StringP("protocol", "p", "json", "Specify the protocol (binary, compact, json, simplejson)")
-	number     = pflag.IntP("number", "c", 0, "Credit Card number")
+	number     = pflag.Int64P("number", "c", 0, "Credit Card number")
 	name       = pflag.StringP("name", "n", "", "Card owner's Name)")
-	crypto     = pflag.IntP("crypto", "", 0, "Ctryptogram")
+	crypto     = pflag.Int32P("crypto", "", 0, "Ctryptogram")
 	date       = pflag.StringP("date", "d", "", "Expiration Date")
 	defaultCtx = context.Background()
 )
+
+const layout = "2006/01/02"
 
 func main() {
 	pflag.Parse()
@@ -30,6 +35,20 @@ func main() {
 	if err != nil {
 		logrus.Fatal(err)
 		os.Exit(1)
+	}
+
+	t, err := time.Parse(layout, *date)
+
+	if err != nil {
+		logrus.Fatal(err)
+		os.Exit(1)
+	}
+
+	client.Card = payment.CreditCard{
+		Number:     *number,
+		Cryptogram: *crypto,
+		Holder:     *name,
+		Date:       t.Unix(),
 	}
 
 	spew.Dump(client.Authorize(defaultCtx))
