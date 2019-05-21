7a79f39e9c37dc4d75d4b6274cb67b3f786505c4
melkor <melkor666free@gmail.com>
2019-05-21 10:54:31 +0200

    add CreditCard parmeter into server

--- server/handler.go
+++ server/handler.go
@@ -1,6 +1,10 @@
 package server
 
-import "context"
+import (
+	"context"
+
+	"github.com/melkor/test-thrift/payment"
+)
 
 type PaymentHandler struct {
 }
@@ -9,10 +13,10 @@ func NewPaymentHandler() *PaymentHandler {
 	return &PaymentHandler{}
 }
 
-func (p *PaymentHandler) Authorize(c context.Context) (bool, error) {
+func (p *PaymentHandler) Authorize(c context.Context, card *payment.CreditCard) (bool, error) {
 	return true, nil
 }
 
-func (p *PaymentHandler) Pay(c context.Context, amount int32) (bool, error) {
+func (p *PaymentHandler) Pay(c context.Context, card *payment.CreditCard, amount int32) (bool, error) {
 	return true, nil
 }
